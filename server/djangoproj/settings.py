f4edf41e719045f1e7104ac654866dac02550842
domyn84 <okostiuk.tech@gmail.com>
2025-06-07 21:02:13 -0700

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,13 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-    'https://okostiuktech-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    "https://okostiuktech-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy."
+    "cognitiveclass.ai/"
+]
 CSRF_TRUSTED_ORIGINS = [
-    'https://okostiuktech-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    "https://okostiuktech-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy."
+    "cognitiveclass.ai/"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,7 +100,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
     },
     {
         'NAME':
@@ -145,4 +150,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-         
