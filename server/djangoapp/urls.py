90f731c4bc5a8ae18047b60766d3c269e0393aba
domyn84 <okostiuk.tech@gmail.com>
2025-06-07 18:58:59 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     # path for dealer reviews view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, 
+    path(route='get_dealers/<str:state>', view=views.get_dealerships,
          name='get_dealers_by_state'),
-    path(route='reviews/dealer/<int:dealer_id>', 
+    path(route='reviews/dealer/<int:dealer_id>',
          view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
     path(route='get_cars', view=views.get_cars, name='getcars'),
